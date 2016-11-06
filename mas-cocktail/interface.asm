c25d82f0fdd0188be26d05b7c4b0252fcb6eda7f
GAlcaraz <g.alcaraz@outlook.com>
2016-11-06 02:10:09 -0300

    Menu (muy) rudimentario de prueba

--- mas-cocktail/interface.asm
+++ mas-cocktail/interface.asm
@@ -26,6 +26,66 @@ DisplayWelcome:
 		
 	ret
 
+DisplayMenu0:
+	
+	LDi ZH, High(2*T_Mode_Select)
+	LDI ZL, LOW(2*T_Mode_Select)
+	
+	RCALL DisplayString
+	
+		
+	ret
+
+DisplayMenu1a:
+	
+	LDi ZH, High(2*T_Pred_Select)
+	LDI ZL, LOW(2*T_Pred_Select)
+	
+	RCALL DisplayString
+	
+		
+	ret
+
+DisplayMenu2a:
+	
+	LDi ZH, High(2*T_Pot_Select)
+	LDI ZL, LOW(2*T_Pot_Select)
+	
+	RCALL DisplayString
+	
+		
+	ret
+
+DisplayMenu1b:
+	
+	LDi ZH, High(2*T_Bebida1_Select)
+	LDI ZL, LOW(2*T_Bebida1_Select)
+	
+	RCALL DisplayString
+	
+		
+	ret
+
+DisplayMenu2b:
+	
+	LDi ZH, High(2*T_Porc_Select)
+	LDI ZL, LOW(2*T_Porc_Select)
+	
+	RCALL DisplayString
+	
+		
+	ret
+
+DisplayMenu3b:
+	
+	LDi ZH, High(2*T_Bebida2_Select)
+	LDI ZL, LOW(2*T_Bebida2_Select)
+	
+	RCALL DisplayString
+	
+		
+	ret
+
  ;....................TABLAS (display)...........................
 
 T_Welcome:
@@ -36,3 +96,18 @@ T_COCKTail:
 	
 T_Mode_Select:
 	.DB		"Seleccione modo de operación:\n1-Predeterminado\t2-Precisión ",0 
+
+T_Pred_Select:
+	.DB		"Seleccione bebida:\n1-Fernet\t2-Whiscola\t3-Coca-Cola",0
+
+T_Pot_Select:
+	.DB		"Seleccione potencia:\n1-Fuerte\t2-Media\t3-Me la como",0
+
+T_Bebida1_Select:
+	.DB		"Seleccione primera bebida:\n1-Fernet\t2-Whisky\t3-Coca ",0
+
+T_Bebida2_Select:
+	.DB		"Seleccione segunda bebida:\n1-Fernet\t2-Whisky\t3-Coca ",0
+
+T_Porc_Select:
+	.DB		"Introduzca porcentaje: ",0
